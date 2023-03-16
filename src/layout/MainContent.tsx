10d885f84b57b6d2f620ec84ef2072a899f1912b
BUMBAIYA <amitchauhan263871@gmail.com>
2023-03-17 02:06:08 +0530

    fic for html main to take whole height of veiwport

--- src/layout/MainContent.tsx
+++ src/layout/MainContent.tsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 
 export default function MainContent() {
   return (
-    <main className="flex-1 py-6 sm:py-10">
+    <main className="flex flex-1 flex-col py-6 sm:py-10">
       <Outlet />
     </main>
   );
