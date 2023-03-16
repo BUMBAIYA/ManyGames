c5710990658b45820a7322ab8075f3cd17df33d9
BUMBAIYA <amitchauhan263871@gmail.com>
2023-03-16 19:23:31 +0530

    useEvent hook include ref

--- src/hooks/useEvent.ts
+++ src/hooks/useEvent.ts
@@ -1,16 +1,24 @@
-import { useEffect } from "react";
+import { useEffect, RefObject } from "react";
 
-type EventType = "keydown" | "keypres";
+type EventType = keyof HTMLElementEventMap;
 
-export default function useEvent(
-  event: EventType,
-  handler: any,
-  passive = false
+export default function useEvent<
+  T extends HTMLElement | HTMLElement,
+  K extends EventType
+>(
+  ref: RefObject<T> | HTMLElement,
+  eventName: K,
+  handler: (event: HTMLElementEventMap[K]) => void,
+  options?: boolean | AddEventListenerOptions
 ) {
   useEffect(() => {
-    window.addEventListener(event, handler, passive);
-    return () => {
-      window.removeEventListener(event, handler);
-    };
-  });
+    const node = "current" in ref ? ref.current : ref;
+    if (node) {
+      node.addEventListener(eventName, handler, options);
+      return () => {
+        node.removeEventListener(eventName, handler, options);
+      };
+    }
+    return;
+  }, [eventName, handler, options, ref]);
 }
