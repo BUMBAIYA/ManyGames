281b10bd6ef7a2dc68257cdea7f4326cba39be89
BUMBAIYA <amitchauhan263871@gmail.com>
2023-03-17 18:00:36 +0530

    removed error message for game loading

--- src/layout/GameError.tsx
+++ src/layout/GameError.tsx
@@ -1,18 +1,10 @@
 import { Link } from "react-router-dom";
-import { useRouteError } from "react-router-dom";
-
 export default function GameError() {
-  const error: any = useRouteError();
-
   return (
-    <div className="flex flex-1 flex-col items-center justify-center text-zinc-900 dark:text-emerald-400">
-      <h1>Oops!</h1>
+    <div className="flex flex-1 flex-col items-center justify-center px-4 text-center text-zinc-900 dark:text-emerald-400">
+      <h1 className="text-xl font-bold text-emerald-500">Oops!</h1>
       <p>Sorry, an unexpected error has occurred while loading game.</p>
-      <p>
-        <i>{error.statusText || error.message}</i>
-      </p>
-      <div className="mt-4"></div>
-      <Link to="/" className="text-blue-500 underline">
+      <Link to="/" className="mt-4 text-blue-500 underline">
         Return to home
       </Link>
     </div>
