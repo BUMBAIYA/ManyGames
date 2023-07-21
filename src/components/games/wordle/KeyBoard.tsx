d1a5af8497989c7df130d1298ac214f184a8e013
Amit Chauhan <amitchauhan263871@gmail.com>
2023-07-21 08:06:55 +0530

    wordle keyboard icons on mobile

--- src/components/games/wordle/KeyBoard.tsx
+++ src/components/games/wordle/KeyBoard.tsx
@@ -1,4 +1,5 @@
 import { classNames } from "../../../utility/css";
+import { BackspaceIcon, PaperAirplaneIcon } from "@heroicons/react/24/outline";
 
 export type KeyBoardResponse = {
   key: string;
@@ -60,11 +61,23 @@ export function KeyBoard(props: IKeyBoardProps) {
         })}
       </div>
       <div className="flex gap-1 md:gap-1.5">
-        <Key value={"Enter"} isDisable={false} />
+        <button
+          className="flex cursor-pointer items-center justify-center rounded-md border border-emerald-600 bg-emerald-100 p-2 text-xs font-medium text-emerald-600 hover:bg-emerald-200 hover:text-emerald-800 dark:border-emerald-700 dark:bg-emerald-400 dark:text-zinc-900   dark:hover:bg-emerald-400/40 sm:rounded-lg sm:px-3 sm:py-2 sm:text-base md:py-1 lg:px-4 lg:py-1 lg:text-xl"
+          onClick={() => handleSubmitKey("Enter")}
+        >
+          <span className="hidden md:inline-block">ENTER</span>
+          <PaperAirplaneIcon className="h-5 w-5" />
+        </button>
         {BottomLevelLKeys.map((key, index) => {
           return <Key key={index} value={key} isDisable={isKeyDisabled(key)} />;
         })}
-        <Key value={"Backspace"} isDisable={false} />
+        <button
+          className="flex cursor-pointer items-center justify-center rounded-md border border-emerald-600 bg-emerald-100 p-2 text-xs font-medium text-emerald-600 hover:bg-emerald-200 hover:text-emerald-800 dark:border-emerald-700 dark:bg-emerald-400 dark:text-zinc-900   dark:hover:bg-emerald-400/40 sm:rounded-lg sm:px-3 sm:py-2 sm:text-base md:py-1 lg:px-4 lg:py-1 lg:text-xl"
+          onClick={() => handleSubmitKey("Backspace")}
+        >
+          <span className="hidden md:inline-block">BACKSPACE</span>
+          <BackspaceIcon className="h-5 w-5" />
+        </button>
       </div>
     </div>
   );
