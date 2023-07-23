2a4d08191b4c16fafa29ccf669229555fa14be16
Amit Chauhan <amitchauhan263871@gmail.com>
2023-07-24 04:04:32 +0530

    pwa support

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,55 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA, VitePWAOptions } from "vite-plugin-pwa";
+
+const manifestForPlugin: Partial<VitePWAOptions> = {
+  registerType: "prompt",
+  includeAssets: [
+    "favicon.ico",
+    "/icons/apple-touch-icon.png",
+    "masked-icon.svg",
+  ],
+  manifest: {
+    name: "Many Games",
+    short_name: "ManyGames",
+    description: "App to play puzzle and memory games",
+    icons: [
+      {
+        src: "/icons/android-chrome-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+        purpose: "monochrome",
+      },
+      {
+        src: "/icons/android-chrome-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "any",
+      },
+      {
+        src: "/icons/apple-touch-icon.png",
+        sizes: "180x180",
+        type: "image/png",
+        purpose: "apple touch icon",
+      },
+      {
+        src: "/icons/maskable-icon.png",
+        sizes: "384x384",
+        type: "image/png",
+        purpose: "any maskable",
+      },
+    ],
+    theme_color: "#10b981",
+    background_color: "#18181b",
+    display: "standalone",
+    scope: "/",
+    start_url: "/",
+    orientation: "portrait",
+  },
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: "./",
+  plugins: [react(), VitePWA(manifestForPlugin)],
 });
