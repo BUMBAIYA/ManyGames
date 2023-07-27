39071f11d17d591839846b15e09d4d72adf045a9
Amit Chauhan <amitchauhan263871@gmail.com>
2023-07-27 06:41:20 +0530

    force update app on new version

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes";
 import "./styles/global/main.css";
+import { registerSW } from "virtual:pwa-register";
+
+registerSW({ immediate: true });
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
