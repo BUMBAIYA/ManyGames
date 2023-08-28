4f5374572d50b32d60d383b8a9b3b5804bd65ab6
Amit Chauhan <amitchauhan263871@gmail.com>
2023-08-28 23:57:10 +0530

    board size handler memory match

--- src/components/games/memoryMatch/GameBoard.tsx
+++ src/components/games/memoryMatch/GameBoard.tsx
@@ -1,11 +1,11 @@
-import { useEffect, useState } from "react";
-import styles from "./styles.module.css";
-import { MemoryMatchTile, generateNewTiles, getRandomLetters } from "./helper";
+import { useEffect, useRef, useState } from "react";
+import { MemoryMatchTile, generateNewTiles } from "./helper";
 import {
   ArrowPathIcon,
   Cog6ToothIcon,
   InformationCircleIcon,
 } from "@heroicons/react/24/outline";
+import styles from "./styles.module.css";
 
 const VALID_LETTERS: string[] = [
   "🍅",
@@ -25,25 +25,71 @@ const VALID_LETTERS: string[] = [
   "🍆",
 ];
 
+type visitedLetter = {
+  commonID: number;
+};
+
 export default function MemoryMatchBoard() {
+  const refBoard = useRef<HTMLDivElement>(null);
   const [tiles, setTiles] = useState<MemoryMatchTile[]>([]);
+  const [visitedLetter, setVisitedLetter] = useState<visitedLetter[]>([]);
+  const [correctGuessedID, setCorrectGuessedID] = useState<number[]>([]);
+
+  const setVisible = (id: string) => {
+    setTiles((prev) =>
+      prev.map((tile) => {
+        return tile.id === id
+          ? {
+              ...tile,
+              isCurrentlyVisible: true,
+              isVisited: true,
+            }
+          : tile;
+      }),
+    );
+  };
+
+  const handleResetGame = () => {
+    setTiles(generateNewTiles(VALID_LETTERS, 5));
+  };
+
+  const handleBoardSize = (size: number) => {
+    refBoard.current?.style.setProperty("--board-size", `${size}`);
+    setTiles(generateNewTiles(VALID_LETTERS, size));
+  };
 
   useEffect(() => {
     setTiles(generateNewTiles(VALID_LETTERS, 5));
   }, []);
 
+  useEffect(() => {
+    console.log(tiles);
+  }, [tiles]);
+
   return (
     <div className="flex w-full flex-col-reverse items-center gap-4 lg:flex-row lg:items-start lg:justify-center">
-      <div className={`${styles.board_grid}`}>
+      <div ref={refBoard} className={`${styles.board_grid}`}>
         {tiles.map((value, index) => {
           return (
             <button
               type="button"
-              className="relative inline-flex h-full w-full rounded-md border-2 border-emerald-500/40 bg-white p-2 shadow-sm transition-shadow duration-200 hover:shadow-lg dark:bg-zinc-700 dark:hover:shadow-emerald-400/40"
+              className="relative inline-flex h-full w-full rounded-md border-2 border-emerald-500/40 bg-white p-2 shadow-sm transition-shadow duration-200 hover:shadow-lg dark:bg-zinc-900 dark:hover:shadow-emerald-400/40"
+              style={
+                value.isCurrentlyVisible
+                  ? {
+                      animation: `500ms linear 100ms ${styles.spinx}`,
+                    }
+                  : {
+                      animation: `400ms linear ${(index % 6) * 40}ms ${
+                        styles.reset_animation
+                      }`,
+                    }
+              }
               key={index}
+              onClick={() => setVisible(value.id)}
             >
               <span className="flex h-full w-full items-center justify-center">
-                {value.name}
+                {!value.isCurrentlyVisible ? value.name : null}
               </span>
             </button>
           );
@@ -55,6 +101,7 @@ export default function MemoryMatchBoard() {
             aria-label="How to play"
             type="button"
             className="flex h-10 w-10 items-center justify-center rounded-md transition hover:bg-zinc-900/5 dark:hover:bg-white/5"
+            onClick={handleResetGame}
           >
             <ArrowPathIcon className="h-8 w-8 stroke-zinc-900 dark:stroke-emerald-300" />
           </button>
