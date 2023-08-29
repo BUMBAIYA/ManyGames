d714e0513f92ed58da87fa600a12f452a1fd4673
Amit Chauhan <amitchauhan263871@gmail.com>
2023-08-30 04:08:09 +0530

    memory match setting modal

--- src/components/games/memoryMatch/GameBoard.tsx
+++ src/components/games/memoryMatch/GameBoard.tsx
@@ -1,12 +1,15 @@
-import { useEffect, useRef, useState } from "react";
-import { MemoryMatchTile, generateNewTiles } from "./helper";
+import { useEffect, useLayoutEffect, useRef, useState } from "react";
 import {
   ArrowPathIcon,
   Cog6ToothIcon,
   InformationCircleIcon,
 } from "@heroicons/react/24/outline";
-import styles from "./styles.module.css";
+import { MemoryMatchHelper as MMH } from "./helper";
 import useLocalStorage from "../../../hooks/useLocalStorage";
+import { BasicModal } from "../../modal/BasicModal";
+import { MemoryMatchSettingModal } from "./SettingModal";
+import ConfettiComponent from "../../modal/ConfettiComponent";
+import styles from "./styles.module.css";
 
 const VALID_LETTERS: string[] = [
   "🍅",
@@ -26,33 +29,78 @@ const VALID_LETTERS: string[] = [
   "🍆",
 ];
 
+type BoardGridCSS = {
+  gridSize: string;
+  fontSize: string;
+};
+
+type MemoryMatchStore = {
+  cs: number;
+  ls: number | null;
+};
+
 export default function MemoryMatchBoard() {
   const [boardSize, setBoardSize] = useState<number>(5);
-  const [lost, setLost] = useState<boolean>(false);
-  const [score, setScore] = useLocalStorage<number>("memory-match", 0);
+  const [isGameLost, setGameLost] = useState<boolean>(false);
+  const [isGameWon, setIsGameWon] = useState<boolean>(false);
+  const [openWonLostModal, setWonLostModal] = useState<boolean>(false);
+  const [openSettingModal, setOpenSettingModal] = useState<boolean>(false);
+  const [boardStore, setBoardStore] = useLocalStorage<MemoryMatchStore>(
+    "memory-match",
+    {
+      cs: 0,
+      ls: null,
+    },
+  );
   const refBoard = useRef<HTMLDivElement>(null);
-  const [tiles, setTiles] = useState<MemoryMatchTile[]>([]);
-  const [currentTile, setCurrentTile] = useState<MemoryMatchTile | null>(null);
+  const [tiles, setTiles] = useState<MMH.MemoryMatchTile[]>([]);
+  const [currentTile, setCurrentTile] = useState<MMH.MemoryMatchTile | null>(
+    null,
+  );
   const [correctGuessedID, setCorrectGuessedID] = useState<Set<number>>(
     new Set(),
   );
 
-  const handleSetVisible = (tile: MemoryMatchTile) => {
+  const checkIsGameLost = (tile: MMH.MemoryMatchTile) => {
+    if (tile.commonID === 0 && tile.isVisited === true) {
+      setGameLost(true);
+      setWonLostModal(true);
+      setTiles((prev) =>
+        prev.map((t) => {
+          return t.id === tile.id || t.isCorrectGuessed === true
+            ? { ...t, isCurrentlyVisible: true }
+            : { ...t, isCurrentlyVisible: false };
+        }),
+      );
+    }
+  };
+
+  const flipConsercativeBomb = (
+    currentTile: MMH.MemoryMatchTile,
+    tile: MMH.MemoryMatchTile,
+  ) => {
+    if (currentTile.commonID === 0 && tile.commonID === 0) {
+      setCurrentTile(tile);
+      setTiles((prev) =>
+        prev.map((t) => {
+          return t.id === tile.id
+            ? { ...t, isCurrentlyVisible: true }
+            : t.isCorrectGuessed
+            ? t
+            : { ...t, isCurrentlyVisible: false };
+        }),
+      );
+    }
+  };
+
+  const handleSetVisible = (tile: MMH.MemoryMatchTile) => {
     if (currentTile) {
       if (currentTile.id === tile.id) return;
-      setScore((prev) => prev + 1);
-      if (tile.commonID === 0 && tile.isVisited === true) {
-        setLost(true);
-        setTiles((prev) =>
-          prev.map((t) => {
-            return t.id === tile.id || t.isCorrectGuessed === true
-              ? { ...t, isCurrentlyVisible: true }
-              : { ...t, isCurrentlyVisible: false };
-          }),
-        );
-      }
+      incrementScore();
+      checkIsGameLost(tile);
+      flipConsercativeBomb(currentTile, tile);
       if (currentTile.commonID === tile.commonID) {
-        if (hasInvisibleTileWithCommonID(currentTile.commonID, tile)) {
+        if (hasInvisibleTileWithCommonID(currentTile.commonID, tile.id)) {
           setCurrentTile(tile);
           setTiles((prev) =>
             prev.map((t) => {
@@ -66,6 +114,15 @@ export default function MemoryMatchBoard() {
             }),
           );
         } else {
+          const isWon = handleGameWon(tile.commonID);
+          if (isWon) {
+            setTiles((prev) =>
+              prev.map((t) => {
+                return { ...t, isCurrentlyVisible: true };
+              }),
+            );
+            return;
+          }
           setCurrentTile(tile);
           setCorrectGuessedID((prev) => prev.add(tile.commonID));
           setTiles((prev) =>
@@ -99,7 +156,7 @@ export default function MemoryMatchBoard() {
       }
     } else {
       setCurrentTile(tile);
-      setScore((prev) => prev + 1);
+      incrementScore();
       setTiles((prev) =>
         prev.map((t) => {
           return t.id === tile.id
@@ -114,92 +171,211 @@ export default function MemoryMatchBoard() {
     }
   };
 
+  const incrementScore = () => {
+    setBoardStore((prev) => {
+      return { ...prev, cs: prev.cs + 1 };
+    });
+  };
+
   const hasInvisibleTileWithCommonID = (
     commonID: number,
-    currentTile: MemoryMatchTile,
+    currentTileID: string,
   ): boolean => {
     return tiles.some((tile) =>
-      currentTile.id === tile.id
+      currentTileID === tile.id
         ? false
         : tile.commonID === commonID && !tile.isCurrentlyVisible,
     );
   };
 
+  const handleGameWon = (currentCommonID: number) => {
+    if (
+      !tiles.some((t) =>
+        t.commonID === 0
+          ? false
+          : t.commonID === currentCommonID
+          ? false
+          : !t.isCorrectGuessed,
+      )
+    ) {
+      setIsGameWon(true);
+      setWonLostModal(true);
+      return true;
+    }
+    return false;
+  };
+
   const handleResetGame = () => {
-    setTiles(generateNewTiles(VALID_LETTERS, boardSize));
+    handleBoardSize(boardSize);
+    setTiles(MMH.generateNewTiles(VALID_LETTERS, boardSize));
     setCorrectGuessedID(new Set<number>());
     setCurrentTile(null);
-    setLost(false);
+    setGameLost(false);
+    setIsGameWon(false);
+    setBoardStore((prev) => {
+      return { ...prev, cs: 0 };
+    });
   };
 
   const handleBoardSize = (size: number) => {
     setBoardSize(size);
-    refBoard.current?.style.setProperty("--board-size", `${boardSize}`);
-    setTiles(generateNewTiles(VALID_LETTERS, boardSize));
+    setBoardCssVariables(
+      size,
+      MMH.MEMORY_MATCH_BOARD_CONFIG[size].gridCSSVar.gridMobile,
+      MMH.MEMORY_MATCH_BOARD_CONFIG[size].gridCSSVar.gridDesktop,
+    );
+    setTiles(MMH.generateNewTiles(VALID_LETTERS, boardSize));
   };
 
+  const setBoardCssVariables = (
+    size: number,
+    gridMobile: BoardGridCSS,
+    gridDesktop: BoardGridCSS,
+  ) => {
+    refBoard.current?.style.setProperty("--board-size", `${size}`);
+    refBoard.current?.style.setProperty(
+      "--grid-size",
+      window.innerWidth <= 640 ? gridMobile.gridSize : gridDesktop.gridSize,
+    );
+    refBoard.current?.style.setProperty(
+      "--font-size",
+      window.innerWidth <= 640 ? gridMobile.fontSize : gridDesktop.fontSize,
+    );
+  };
+
+  const resetBoardCssVar = () => {
+    setBoardCssVariables(
+      boardSize,
+      MMH.MEMORY_MATCH_BOARD_CONFIG[boardSize].gridCSSVar.gridMobile,
+      MMH.MEMORY_MATCH_BOARD_CONFIG[boardSize].gridCSSVar.gridDesktop,
+    );
+  };
+
+  const handleCloseWonLostModal = () => {
+    setWonLostModal(false);
+    handleResetGame();
+  };
+
+  useLayoutEffect(() => {
+    resetBoardCssVar();
+    setTiles(MMH.generateNewTiles(VALID_LETTERS, boardSize));
+    window.addEventListener("resize", resetBoardCssVar);
+    return () => window.removeEventListener("resize", resetBoardCssVar);
+  }, [boardSize]);
+
   useEffect(() => {
-    handleBoardSize(boardSize);
-  }, []);
+    handleResetGame();
+  }, [boardSize]);
+
+  useEffect(() => {
+    console.log(tiles);
+  }, [tiles]);
 
   return (
-    <div className="flex w-full flex-col-reverse items-center gap-4 lg:flex-row lg:items-start lg:justify-center">
-      <div ref={refBoard} className={`${styles.board_grid}`}>
-        {tiles.map((tile, index) => {
-          return (
+    <>
+      <div className="flex w-full flex-col-reverse items-center gap-4 lg:flex-row lg:items-start lg:justify-center">
+        <div ref={refBoard} className={`${styles.board_grid}`}>
+          {tiles.map((tile, index) => {
+            return (
+              <button
+                type="button"
+                className="relative inline-flex h-full w-full rounded-md border-2 border-emerald-500/40 bg-white p-2 shadow-sm transition-shadow duration-200 hover:shadow-lg dark:bg-zinc-900 dark:hover:shadow-emerald-400/40"
+                style={
+                  tile.isCurrentlyVisible
+                    ? {
+                        animation: `500ms linear 100ms ${styles.spinx}`,
+                      }
+                    : {
+                        animation: `400ms linear ${(index % 6) * 40}ms ${
+                          styles.reset_animation
+                        }`,
+                      }
+                }
+                key={tile.id}
+                onClick={() => handleSetVisible(tile)}
+              >
+                <span className="flex h-full w-full items-center justify-center">
+                  {tile.isCurrentlyVisible ? tile.name : null}
+                </span>
+              </button>
+            );
+          })}
+          {Object.entries(correctGuessedID).map(([key, value]) => {
+            return value;
+          })}
+        </div>
+        <div className="flex w-full justify-end lg:w-auto">
+          <div className="flex gap-2 lg:flex-col">
             <button
+              aria-label="How to play"
               type="button"
-              className="relative inline-flex h-full w-full rounded-md border-2 border-emerald-500/40 bg-white p-2 shadow-sm transition-shadow duration-200 hover:shadow-lg dark:bg-zinc-900 dark:hover:shadow-emerald-400/40"
-              style={
-                tile.isCurrentlyVisible
-                  ? {
-                      animation: `500ms linear 100ms ${styles.spinx}`,
-                    }
-                  : {
-                      animation: `400ms linear ${(index % 6) * 40}ms ${
-                        styles.reset_animation
-                      }`,
-                    }
-              }
-              key={index}
-              onClick={() => handleSetVisible(tile)}
+              className="flex h-10 w-10 items-center justify-center rounded-md transition hover:bg-zinc-900/5 dark:hover:bg-white/5"
+              onClick={handleResetGame}
             >
-              <span className="flex h-full w-full items-center justify-center">
-                {tile.isCurrentlyVisible ? tile.name : null}
-              </span>
+              <ArrowPathIcon className="h-8 w-8 stroke-zinc-900 dark:stroke-emerald-300" />
             </button>
-          );
-        })}
-        {Object.entries(correctGuessedID).map(([key, value]) => {
-          return value;
-        })}
-      </div>
-      <div className="flex w-full justify-end lg:w-auto">
-        <div className="flex gap-2 lg:flex-col">
-          <button
-            aria-label="How to play"
-            type="button"
-            className="flex h-10 w-10 items-center justify-center rounded-md transition hover:bg-zinc-900/5 dark:hover:bg-white/5"
-            onClick={handleResetGame}
-          >
-            <ArrowPathIcon className="h-8 w-8 stroke-zinc-900 dark:stroke-emerald-300" />
-          </button>
-          <button
-            aria-label="How to play"
-            type="button"
-            className="flex h-10 w-10 items-center justify-center rounded-md transition hover:bg-zinc-900/5 dark:hover:bg-white/5"
-          >
-            <Cog6ToothIcon className="h-8 w-8 stroke-zinc-900 dark:stroke-emerald-300" />
-          </button>
-          <button
-            aria-label="How to play"
-            type="button"
-            className="flex h-10 w-10 items-center justify-center rounded-md transition hover:bg-zinc-900/5 dark:hover:bg-white/5"
-          >
-            <InformationCircleIcon className="h-8 w-8 stroke-zinc-900 dark:stroke-emerald-300" />
-          </button>
+            <button
+              aria-label="How to play"
+              type="button"
+              className="flex h-10 w-10 items-center justify-center rounded-md transition hover:bg-zinc-900/5 dark:hover:bg-white/5"
+              onClick={() => setOpenSettingModal(true)}
+            >
+              <Cog6ToothIcon className="h-8 w-8 stroke-zinc-900 dark:stroke-emerald-300" />
+            </button>
+            <MemoryMatchSettingModal
+              isOpen={openSettingModal}
+              closeModal={setOpenSettingModal}
+              size={boardSize}
+              handleSubmit={(size) => {
+                setBoardSize(size);
+              }}
+            />
+            <button
+              aria-label="How to play"
+              type="button"
+              className="flex h-10 w-10 items-center justify-center rounded-md transition hover:bg-zinc-900/5 dark:hover:bg-white/5"
+            >
+              <InformationCircleIcon className="h-8 w-8 stroke-zinc-900 dark:stroke-emerald-300" />
+            </button>
+          </div>
         </div>
       </div>
-    </div>
+      {(isGameLost || isGameWon) && (
+        <BasicModal
+          title={isGameWon ? "You won" : "You lost"}
+          isOpen={openWonLostModal}
+          closeModal={handleCloseWonLostModal}
+          confetti={isGameWon && <ConfettiComponent />}
+        >
+          {isGameLost && (
+            <div className="mt-4">
+              <span className="text-sm font-normal">
+                Opps! you clicked the same bomb{" "}
+                <span className="text-2xl">
+                  {MMH.MEMORY_MATCH_INVALID_TEXT}
+                </span>{" "}
+                twice
+              </span>
+              <br />
+              <span className="text-xs font-semibold text-emerald-600 dark:text-emerald-400">
+                Hint: Always remember the bomb's position
+              </span>
+            </div>
+          )}
+          {isGameWon && (
+            <div className="mt-4">
+              <p className="mb-1 text-sm text-gray-500">
+                {`${
+                  boardStore.cs < boardStore.ls! ? "New Lowest Score" : "Score"
+                } for size ${boardSize} board`}
+              </p>
+              <p className="text-3xl font-bold text-emerald-500">
+                {boardStore.cs}
+              </p>
+            </div>
+          )}
+        </BasicModal>
+      )}
+    </>
   );
 }
--- src/components/games/memoryMatch/helper.ts
+++ src/components/games/memoryMatch/helper.ts
@@ -1,85 +1,123 @@
 import { shuffleTiles } from "../../../utility/shuffle";
-import { s4 } from "../../../utility/uuidGenerator";
+import { uuid4 } from "../../../utility/uuidGenerator";
 
-export type MemoryMatchTile = {
-  name: string;
-  id: string;
-  commonID: number;
-  isVisited: boolean;
-  isCurrentlyVisible: boolean;
-  isDisabled: boolean;
-  isKill: boolean;
-  isCorrectGuessed: boolean;
-};
+export namespace MemoryMatchHelper {
+  export type MemoryMatchTile = {
+    name: string;
+    id: string;
+    commonID: number;
+    isVisited: boolean;
+    isCurrentlyVisible: boolean;
+    isDisabled: boolean;
+    isKill: boolean;
+    isCorrectGuessed: boolean;
+  };
 
-const INVALID_TEXT = "☠️" as const;
+  export const MEMORY_MATCH_INVALID_TEXT = "☠️" as const;
 
-type MemoryBoardConfig = {
-  duplicate: number;
-  wrongDuplicate: number;
-};
+  type MemoryBoardConfig = {
+    duplicate: number;
+    wrongDuplicate: number;
+    gridCSSVar: {
+      gridMobile: {
+        gridSize: string;
+        fontSize: string;
+      };
+      gridDesktop: {
+        gridSize: string;
+        fontSize: string;
+      };
+    };
+  };
 
-export const BOARD_CONFIG: Record<number, MemoryBoardConfig> = {
-  4: {
-    duplicate: 2,
-    wrongDuplicate: 0,
-  },
-  5: {
-    duplicate: 2,
-    wrongDuplicate: 1,
-  },
-  6: {
-    duplicate: 3,
-    wrongDuplicate: 3,
-  },
-};
+  export const MEMORY_MATCH_BOARD_CONFIG: Record<number, MemoryBoardConfig> = {
+    4: {
+      duplicate: 2,
+      wrongDuplicate: 0,
+      gridCSSVar: {
+        gridMobile: {
+          gridSize: "21vw",
+          fontSize: "10vw",
+        },
+        gridDesktop: {
+          gridSize: "15vh",
+          fontSize: "7vh",
+        },
+      },
+    },
+    5: {
+      duplicate: 2,
+      wrongDuplicate: 1,
+      gridCSSVar: {
+        gridMobile: {
+          gridSize: "16vw",
+          fontSize: "8vw",
+        },
+        gridDesktop: {
+          gridSize: "12vh",
+          fontSize: "6vh",
+        },
+      },
+    },
+    6: {
+      duplicate: 3,
+      wrongDuplicate: 3,
+      gridCSSVar: {
+        gridMobile: {
+          gridSize: "14vw",
+          fontSize: "7vw",
+        },
+        gridDesktop: {
+          gridSize: "12vh",
+          fontSize: "5vh",
+        },
+      },
+    },
+  } as const;
 
-export const getRandomLetters = (
-  arr: string[],
-  size: number,
-  length: number,
-) => {
-  const shuffledString = shuffleTiles(arr);
-  return shuffledString.slice(0, length);
-};
+  export const getRandomLetters = (arr: string[], length: number) => {
+    const shuffledString = shuffleTiles(arr);
+    return shuffledString.slice(0, length);
+  };
 
-export function generateNewTiles(arr: string[], size: number) {
-  let tiles: MemoryMatchTile[] = [];
-  let randomTiles: string[] = [];
-  // if (size === 4) {
-  //   randomTiles = getRandomLetters(
-  //     arr,
-  //     size,
-  //     ((size * size) - BOARD_CONFIG[size].wrongDuplicate) / BOARD_CONFIG[size].duplicate,
-  //   );
-  // } else if (size === 5) {
-  //   randomTiles = getRandomLetters(
-  //     arr,
-  //     size,
-  //     (size * size - BOARD_CONFIG[size].wrongDuplicate) /
-  //       BOARD_CONFIG[size].duplicate,
-  //   );
-  // } else if (size === 6) {
+  export function generateNewTiles(arr: string[], size: number) {
+    let tiles: MemoryMatchTile[] = [];
+    let randomTiles: string[] = [];
 
-  // }
+    randomTiles = getRandomLetters(
+      arr,
+      (size * size - MEMORY_MATCH_BOARD_CONFIG[size].wrongDuplicate) /
+        MEMORY_MATCH_BOARD_CONFIG[size].duplicate,
+    );
 
-  randomTiles = getRandomLetters(
-    arr,
-    size,
-    (size * size - BOARD_CONFIG[size].wrongDuplicate) /
-      BOARD_CONFIG[size].duplicate,
-  );
+    for (let i = 0; i < randomTiles.length; i++) {
+      for (
+        let repeatedTile = 0;
+        repeatedTile < MEMORY_MATCH_BOARD_CONFIG[size].duplicate;
+        repeatedTile++
+      ) {
+        tiles.push({
+          name: randomTiles[i],
+          id: uuid4(),
+          commonID: i + 1,
+          isCurrentlyVisible: false,
+          isDisabled: false,
+          isVisited: false,
+          isKill: false,
+          isCorrectGuessed: false,
+        });
+      }
+    }
 
-  for (let i = 0; i < randomTiles.length; i++) {
     for (
       let repeatedTile = 0;
-      repeatedTile < BOARD_CONFIG[size].duplicate;
+      repeatedTile < MEMORY_MATCH_BOARD_CONFIG[size].wrongDuplicate;
       repeatedTile++
     ) {
       tiles.push({
-        name: randomTiles[i],
-        id: s4(),
-        commonID: i + 1,
+        name: MEMORY_MATCH_INVALID_TEXT,
+        id: uuid4(),
+        commonID: 0,
         isCurrentlyVisible: false,
         isDisabled: false,
         isVisited: false,
@@ -87,23 +125,6 @@ export function generateNewTiles(arr: string[], size: number) {
         isCorrectGuessed: false,
       });
     }
+    return shuffleTiles(tiles);
   }
-
-  for (
-    let repeatedTile = 0;
-    repeatedTile < BOARD_CONFIG[size].wrongDuplicate;
-    repeatedTile++
-  ) {
-    tiles.push({
-      name: INVALID_TEXT,
-      id: s4(),
-      commonID: 0,
-      isCurrentlyVisible: false,
-      isDisabled: false,
-      isVisited: false,
-      isKill: false,
-      isCorrectGuessed: false,
-    });
-  }
-  return shuffleTiles(tiles);
 }
