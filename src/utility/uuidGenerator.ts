4afc42732ffc1cbd2ee7bdf4f1b85a900c38e1ff
Amit Chauhan <amitchauhan263871@gmail.com>
2023-08-30 04:07:53 +0530

    new uuid generator

--- src/utility/uuidGenerator.ts
+++ src/utility/uuidGenerator.ts
@@ -3,3 +3,20 @@ export function s4(): string {
     .toString()
     .substring(1);
 }
+
+export function uuid4(): string {
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  const timestamp = Date.now().toString();
+  const seed = timestamp + Math.random().toString();
+  let id = "";
+  for (let i = 0; i < 2; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    id += characters.charAt(randomIndex);
+  }
+  for (let i = 0; i < 2; i++) {
+    const randomIndex = Math.floor(Math.random() * seed.length);
+    id += seed.charAt(randomIndex);
+  }
+  return id;
+}
